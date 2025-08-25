49cab25d69bd5d31597cce47e32df4b5d2569f38
Ganesh Ghumare <ganeshghumare1997@gmail.com>
2025-08-25 11:16:16 +0530

    Updating the URL for image in Landing page

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -119,7 +119,7 @@ export default function Album() {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image="https://picsum.photos/1920/1080?random"
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
